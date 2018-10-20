621dfcf35e59549fdd381b4e235ab5de14129459
HMZahran <hisham.zahran@extremesolution.com>
2018-10-20 21:58:25 +0200

    Add footer + elixir

--- src/common/card/ListCard.component.js
+++ src/common/card/ListCard.component.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import api from '../../api/api'
 import styles from './Card.module.css';
+import { Elixir } from '../icons/Elixir';
 
 const ListCard = ({ idName, elixirCost, getCardDetails }) => (
     <div className={styles.listCard}>
         <img src={api.getImageURL(idName)} alt='Card-Img' className={styles.listCardImg} onClickCapture={() => getCardDetails(idName)} />
         <span className={styles.listCardText}>{idName.toUpperCase()}</span>
-        <div className={styles.elixir}>{elixirCost}</div>
+        <div className={styles.elixir}>
+            <Elixir value={elixirCost} />
+        </div>
     </div>
 )
 
--- src/screens/deck/DeckFooter.component.js
+++ src/screens/deck/DeckFooter.component.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './Deck.module.css';
 
 const DeckFooter = ({ average }) => (
-    <div>
-        {average}
+    <div className={styles.footer}>
+        <span className={styles.footerText}>{`Average Elixir cost: ${average}`}</span>
     </div>
 )
 
