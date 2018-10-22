8e1286ad02caf6f9012faa5ec6f0e29d9f674006
HMZahran <hisham.zahran@extremesolution.com>
2018-10-23 00:42:29 +0200

    Fix footer issue

--- src/screens/deck/DeckFooter.component.js
+++ src/screens/deck/DeckFooter.component.js
@@ -4,11 +4,12 @@ import { Elixir } from '../../common/icons/Elixir';
 
 const DeckFooter = ({ average }) => (
     <footer className={styles.footer}>
-        <span className={styles.footerText}>{`Average Elixir cost: ${average}`}</span>
+        <span className={`${styles.footerText} ${styles.wrapText}`}>{`Average Elixir cost: `}</span>
+        <span className={styles.footerText}>{average}</span>
         <span className={styles.footerElixir}>
             <Elixir />
         </span>
-        {/* <img src={require('../../assets/images/Sword.png')} className={styles.footerSword} alt="Sword" /> */}
+        <img src={require('../../assets/images/Sword.png')} className={styles.footerSword} alt="Sword" />
     </footer>
 )
 
